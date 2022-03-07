3031f3c1b8702843b16750daef4d52f5c90689e8
jungkihun <khkh0130@gmail.com>
2022-03-07 13:17:13 +0900

    jkh variable

--- src/Ex01.java
+++ src/Ex01.java
@@ -1,2 +1,18 @@
 public class Ex01 {
+    String instanceVariable = "instance";
+    static String CLASS_VARIABLE = "class";
+
+    public static void main(String[] args) {
+        System.out.println(Ex01.CLASS_VARIABLE);
+        Ex01 vars = new Ex01();
+        System.out.println(vars.instanceVariable);
+        System.out.println(vars.CLASS_VARIABLE);
+        vars.someMethod("parameter");
+    }
+
+    public void someMethod(String parameter) {
+        String localVariable = "local";
+        System.out.println("parameter: " + parameter);
+        System.out.println("localVariable: " + localVariable);
+    }
 }
