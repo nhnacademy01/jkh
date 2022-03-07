4a29158a082643dd97ee57d461cf95e451bae831
jungkihun <khkh0130@gmail.com>
2022-03-07 15:25:49 +0900

    jkh ex06

--- src/Ex06.java
+++ src/Ex06.java
@@ -1,10 +1,23 @@
 class PrimitiveTypes {
-        int intDefault;
+    int intDefault;
+    byte byteDefault;
+    short shortDefault;
+    long longDefault;
+    float floatDefault;
+    double doubleDefault;
+    char charDefault;
+    boolean booleanDefault;
 
-
-        public void defaultValues() {
-            System.out.println("intDefault: " + intDefault);
-        }
+    public void defaultValues() {
+        System.out.println("intDefault: " + intDefault);
+        System.out.println("byteDefault: " + byteDefault);
+        System.out.println("shortDefault: " + shortDefault);
+        System.out.println("longDefault: " + longDefault);
+        System.out.println("floatDefault: " + floatDefault);
+        System.out.println("doubleDefault: " + doubleDefault);
+        System.out.println("charDefault: " + charDefault + ": " + (int)charDefault);
+        System.out.println("booleanDefault: " + booleanDefault);
+    }
     }
 public class Ex06 {
 
