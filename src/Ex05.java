1526165c4e697d4766dc6e7fdb65f0fe21a1ca86
jungkihun <khkh0130@gmail.com>
2022-03-07 15:32:38 +0900

    jkh ex07

--- src/Ex05.java
+++ src/Ex05.java
@@ -1,5 +1,5 @@
 public class Ex05 {
-    public static void main(String[] args) {
+   /* public static void main(String[] args) {
         char a = 'A';
         char b = 65;
         char c = '\u0041';
@@ -7,14 +7,16 @@ public class Ex05 {
         System.out.println("a = " + a);
         System.out.println("b = " + b);
         System.out.println("c = " + c);
-        ///char a = 'a';
-        //char b = 'cc';
-        //char c = '홍';
-        //char d = '\u65';
-        char e = '\u2374';
-        //char f = -1;
-        char g = 65_535;
-        //System.out.println(a+b+c+d+e+f+g);
-    }
+        /*
+        char a = 'a';
+        char b = 'cc';
+        char c = '홍';
+        char d = '\u65';
+        /char e = '\u2374';
+        char f = -1;
+        /char g = 65_535;
+        System.out.println(a+b+c+d+e+f+g);
+        */
+    }*/
 
 }
