a099c39124d1c969490a9a00f4d89b7e96f35eae
jungkihun <khkh0130@gmail.com>
2022-03-07 15:40:41 +0900

    jkh bug fix

--- src/Ex05.java
+++ src/Ex05.java
@@ -1,4 +1,5 @@
 public class Ex05 {
+
    /* public static void main(String[] args) {
         char a = 'A';
         char b = 65;
@@ -11,12 +12,12 @@ public class Ex05 {
         char a = 'a';
         char b = 'cc';
         char c = '홍';
-        char d = '\u65';
+        char d = '\u0065';
         /char e = '\u2374';
         char f = -1;
         /char g = 65_535;
         System.out.println(a+b+c+d+e+f+g);
         */
-   // }*/
+    // }*/
 
 }
