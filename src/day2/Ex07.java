f63a94f16d7eeaff078016b3a84b43b815dbdee8
jungkihun <khkh0130@gmail.com>
2022-03-08 11:17:13 +0900

    jkh ex07

---
+++ src/day2/Ex07.java
@@ -0,0 +1,27 @@
+package day2;
+
+public class Ex07 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("월을 입력하세요: ");
+        int month = scanner.nextInt();
+        printDaysInMonth(month);
+    }
+
+    private static void printDaysInMonth(int month) {
+        switch (month) {
+            case 1: case 3: case 5: case 7: case 8: case 10: case 12:
+                System.out.println(month + " has 31 days.");
+                break;
+            case 4: case 6: case 9: case 11:
+                System.out.println(month + " has 30 days.");
+                break;
+            case 2:
+                System.out.println(month + " has 28 or 29 days.");
+                break;
+            default:
+                System.out.println(month + " is not a month.");
+                break;
+        }
+    }
+}
