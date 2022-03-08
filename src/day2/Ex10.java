91861462161b91504b7f13dbeead08ec77bb4f24
jungkihun <khkh0130@gmail.com>
2022-03-08 11:55:28 +0900

    jkh ex10

---
+++ src/day2/Ex10.java
@@ -0,0 +1,27 @@
+package day2;
+
+public class Ex10 {
+    public static void main(String[] args) {
+        int score =91 /10; //->'/';
+
+        switch (score){
+            case 9:
+                System.out.println("a");
+                break;
+            case 8:
+                System.out.println("b");
+                break;
+            case 7:
+                System.out.println("c");
+                break;
+            case 6:
+                System.out.println("d");
+                break;
+            default:
+                System.out.println("f");
+                break;
+
+
+        }
+    }
+}
