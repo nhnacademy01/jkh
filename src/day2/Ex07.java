ca6a315d8c0eeeb2761b3c841e5e043b85c58b3d
jungkihun <khkh0130@gmail.com>
2022-03-08 11:18:46 +0900

    jkh ex07

--- src/day2/Ex07.java
+++ src/day2/Ex07.java
@@ -13,7 +13,10 @@ public class Ex07 {
             case 1: case 3: case 5: case 7: case 8: case 10: case 12:
                 System.out.println(month + " has 31 days.");
                 break;
-            case 4: case 6: case 9: case 11:
+            case 4:
+                case 6:
+                    case 9:
+                        case 11:
                 System.out.println(month + " has 30 days.");
                 break;
             case 2:
