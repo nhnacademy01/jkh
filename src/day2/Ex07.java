efe1584fd2ff50dd76a04b1c0a63b9c7e8622edf
jungkihun <khkh0130@gmail.com>
2022-03-08 11:32:34 +0900

    jkh ex08

--- src/day2/Ex07.java
+++ src/day2/Ex07.java
@@ -1,11 +1,31 @@
 package day2;
 
+import java.util.Scanner;
+
 public class Ex07 {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         System.out.print("월을 입력하세요: ");
         int month = scanner.nextInt();
         printDaysInMonth(month);
+        printDaysInMonthIf(month);
+    }
+
+    private static void printDaysInMonthIf(int month) {
+        if(month == 1 ||month == 3 ||month == 5 ||month == 7 ||month == 8 ||month == 10 ||month == 12){
+            System.out.println(month +" has 31 days.");
+        }
+        else if(month == 4 ||month == 6 ||month == 9 ||month == 11){
+            System.out.println(month + " has 30 days");
+        }
+        else if(month == 2)
+        {
+            System.out.println(month + " has 28 or 29 days");
+        }
+        else
+        {
+            System.out.println(month + " is not a month");
+        }
     }
 
     private static void printDaysInMonth(int month) {
---
+++ src/day2/Ex08.java
@@ -0,0 +1,34 @@
+package day2;
+
+public class Ex08 {
+    public static void main(String[] args) {
+        int memberGrade = 3;
+
+        switch (memberGrade) {
+            case 3:
+                grantVip();
+            case 2:
+                grantGold();
+            case 1:
+                grantSilver();
+            default:
+                noBenefit();
+        }
+    }
+
+    private static void grantVip() {
+        System.out.println("Issue VIP coupons");
+    }
+
+    private static void grantGold() {
+        System.out.println("Issue Gold coupons");
+    }
+
+    private static void grantSilver() {
+        System.out.println("Issue Silver coupons");
+    }
+
+    private static void noBenefit() {
+        // Nothing
+    }
+}
