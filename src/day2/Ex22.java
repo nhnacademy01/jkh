d6773601f583a8abdae7c20f4932a483d67de330
jungkihun <khkh0130@gmail.com>
2022-03-09 17:43:38 +0900

    jkh ex23

--- src/day2/Ex22.java
+++ src/day2/Ex22.java
@@ -3,50 +3,9 @@ package day2;
 import java.util.Random;
 import java.util.Scanner;
 
-class TicTacToe {
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        Random r = new Random();
-        char[][] board = new char[3][3];
-        int a = 0, b = 0;
-        for (int i = 1; i <= 9; i++) {
+public class Ex22 {
+    static void show(char[][] board) {
 
-
-            if (i % 2 == 0) {
-                System.out.println("컴퓨터턴");
-                    a = r.nextInt(3);
-
-                b = r.nextInt(3);
-                //show(a, b);
-            } else {
-                if (sc.hasNext()){
-                    while (true){
-                    a = sc.nextInt();
-                    if(a>=0&&a<3)
-                        break;
-                    }
-                }
-                while (true){
-                    b = sc.nextInt();
-                    if(b>=0&&b<3)
-                        break;
-                }
-                System.out.println("사용자턴 xy");
-                //show(a, b);
-            }
-        }
-
-
-    }
-
-    static void show(char[][] board,int a, int b) {
-
-
-        for (int i = 0; i < board.length; i++) {
-            for (int j = 0; j < board[i].length; j++) {
-                board[i][j] = ' ';
-            }
-        }
 
         for (int i = 0; i < board.length; i++) {
             System.out.println(" " + board[i][0] + " | " + board[i][1] + " | " + board[i][2]);
@@ -56,13 +15,144 @@ class TicTacToe {
         }
     }
 
-    static void paly_check(int a, int b) {
 
-    }
-}
-
-public class Ex22 {
     public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        Random r = new Random();
+        char[][] board = new char[3][3];
+        int a = 0, b = 0;
+        boolean game = true;
+        char playerwin = ' ';
+        show(board);
+        int ja = -1;
+        while (game) {
+            boolean user = true;
+            boolean com = true;
+
+            for (int i = 0; i < board.length; i++) {
+                for (int j = 0; j < board[i].length; j++) {
+                    board[i][j] = ' ';
+                }
+            }
+            int count = 0;
+            for (int i = 0; i < 3; i++) {
+                for (int j = 0; j < 3; j++) {
+                    if (board[i][j] != ' ') {
+                        count++;
+                    }
+
+                }
+            }
+            if (count == 8) {
+                com = false;
+            } else if (count == 9) {
+                user = false;
+            }
+            while (user) {
+                System.out.println("user ");
+                while (true) {
+                    a = sc.nextInt();
+                    b = sc.nextInt();
+                    if (a < 3 && a >= 0 && b < 3 && b >= 0) {
+                        System.out.println("dsx");
+                        break;
+                    }
+
+                }
+                if (board[a][b] != ' ') {
+                    System.out.println("xxxxffx");
+                    continue;
+                } else {
+                    board[a][b] = '0';
+                }
+
+                while (com) {
+                    System.out.println("computer");
+                    ja = r.nextInt(8) + 1; //1이상 n이하의 무작위 정수
+                    switch (ja) {
+                        case 1:
+                            a = 0;
+                            b = 0;
+                            break;
+                        case 2:
+                            a = 0;
+                            b = 1;
+                            break;
+                        case 3:
+                            a = 0;
+                            b = 2;
+                            break;
+                        case 4:
+                            a = 1;
+                            b = 0;
+                            break;
+                        case 5:
+                            a = 1;
+                            b = 1;
+                            break;
+                        case 6:
+                            a = 1;
+                            b = 2;
+                            break;
+                        case 7:
+                            a = 2;
+                            b = 0;
+                            break;
+                        case 8:
+                            a = 2;
+                            b = 1;
+                            break;
+                        case 9:
+                            a = 2;
+                            b = 2;
+                            break;
+                    }
+
+                    if (board[a][b] != ' ') {
+                        continue;
+                    } else {
+                        board[a][b] = 'X';
+                        break;
+                    }
+
+                }
+                show(board);
+
+            }
+            for (int i = 0; i < 3; i++) {
+                if ((board[i][0] == '0' && board[i][1] == '0' && board[i][2] == '0') ||
+                        (board[0][i] == '0' && board[1][i] == '0' && board[2][i] == '0') ||
+                        (board[0][0] == '0' && board[1][1] == '0' && board[2][2] == '0') ||
+                        (board[2][0] == '0' && board[1][1] == '0' && board[0][2] == '0')) {
+                    playerwin = 'y';
+                    game = false;
+                    break;
+                }
+                if ((board[i][0] == 'X' && board[i][1] == 'X' && board[i][2] == 'X') ||
+                        (board[0][i] == 'X' && board[1][i] == 'X' && board[2][i] == 'X') ||
+                        (board[0][0] == 'X' && board[1][1] == 'X' && board[2][2] == 'X') ||
+                        (board[2][0] == 'X' && board[1][1] == 'X' && board[0][2] == 'X')) {
+                    playerwin = 'n';
+                    game = false;
+                    break;
+                }
+            }
+
+        }
+
+        if (playerwin == 'y') {
+            System.out.println("플레이어 승리!");
+        } else if (playerwin == 'n') {
+            System.out.println("컴퓨터 승리!");
+        } else {
+            System.out.println("비겼습니다!");
+        }
+
 
     }
+
 }
+
+
+
+
--- src/day2/Ex23.java
+++ src/day2/Ex23.java
@@ -121,21 +121,110 @@ public class Ex23 {
     }
 
     void gbb() {
-
+        System.out.println("가위(1) 바위(2) 보(3) :" );
+        Scanner sc = new Scanner(System.in);
+        while (true) {
+            int temp = 0;
+            temp = sc.nextInt();
+            switch (temp) {
+                case 1:
+                    System.out.println("당신은 가위(1)입니다.");
+                    break;
+                case 2:
+                    System.out.println("당신은 바위(2)입니다.");
+                    break;
+                case 3:
+                    System.out.println("당신은 보(3)입니다.");
+                    break;
+            }
+            Random r = new Random();
+            int computer = r.nextInt(3) + 1;
+            switch (computer) {
+                case 1:
+                    System.out.println("컴퓨터는 가위(1)입니다.");
+                    break;
+                case 2:
+                    System.out.println("컴퓨터는 바위(2)입니다.");
+                    break;
+                case 3:
+                    System.out.println("컴퓨터는 보(3)입니다.");
+                    break;
+            }
+            if (temp == computer) {
+                System.out.println("비겼습니다. 다시 합니다.");
+            } else if (temp + 1 == computer) {
+                System.out.println("졌습니다.");
+                break;
+            } else if (temp == computer + 1) {
+                System.out.println("이겼쓰ㅃ니따");
+                break;
+            }
+        }
     }
 
     void lotto() {
+        int [] lot = new int[6];
+        Random r = new Random();
+        for (int i = 0; i < 6; i++) {
+            lot[i] = r.nextInt(45)+1;
+            for (int j = 0; j <i ; j++) {
 
-    }
+                if(lot[i]==lot[j]) {
+                    i--;
+                }
+                }
+            }
+        }
+
+
+
 
     void card() {
+        int [][] arr = new int[5][13];
+        for (int i = 0; i < 5; i++) {
+            for (int j = 0; j <13 ; j++) {
+                arr[i][j] = i +j;
+            }
+        }
+        Random r = new Random();
+        int size = 2;
+        int [] joker = new int[size];
+        int []arr1 = new int[size];
+        for (int i = 0; i < size ; i++) {
+            for (int j = 0; j <2 ; j++) {
+                int x = r.nextInt(5);
+                int y = r.nextInt(13);
+                if(x==4) {
+                    joker[i] = 1;
+                    System.out.println(x);
+                }
+                arr1[i] += arr[x][y];
+            }
+        }
+        int max =0;
 
+        for (int i = 0; i <size ; i++) {
+            if(joker[i] == 1 ) {
+                System.out.printf("win %c",'A'+i);
+                return;
+            }
+            for (int j = 0; j < size ; j++) {
+                if (max < arr1[j]) {
+                    max = arr1[j];
+                }
+
+            }
+            if(arr1[i] == max)
+                System.out.printf("win %c",'A'+i);
+        }
     }
 
     public static void main(String[] args) {
         Ex23 i = new Ex23();
         //i.add();
         // i.nusangi();
-        i.dice();
+        //i.dice();
+      //  i.card();
+
     }
 }
