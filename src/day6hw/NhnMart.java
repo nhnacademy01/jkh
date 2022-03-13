1cd64319a66e934132e993526fd2bb8535b361b9
jungkihun <khkh0130@gmail.com>
2022-03-13 22:25:31 +0900

    jkh day6 Mart_HomeWork

--- src/day6hw/NhnMart.java
+++ src/day6hw/NhnMart.java
@@ -21,10 +21,11 @@ class NhnMartShell {
     private static BuyList inputBuyListFromShell() {
         // Scanner에서 buyList 만들기
         System.out.println("NHN 마트에 오신 것을 환영합니다. 사고 싶은 물건을 골라주세요(2종류만)");
-      //  BuyList buyList = new BuyList();
-       // buyList.add(new BuyList.Item("양파", 2));
-       // buyList.add(new BuyList.Item("계란", 3));
-        // TODO
+        BuyList buyList = new BuyList();
+//        buyList.add(new BuyList.Item("양파", 2));
+//        buyList.add(new BuyList.Item("계란", 2));
+//        buyList.add(new BuyList.Item("파", 1));
+        // TODO scanner 버퍼문제 해결해안됨..
         Scanner sc = new Scanner(System.in);
         BuyList bl = new BuyList();
         String tempName = "";
@@ -32,6 +33,7 @@ class NhnMartShell {
         System.out.println("구매 물건과 수량을 입력해주세요");
         for (int i = 0; i < 2; i++) {
             tempName = sc.nextLine();
+            sc.nextLine();
             tempCount = sc.nextInt();
             System.out.println(tempName +" | "+tempCount);
             bl.add(new BuyList.Item(tempName,tempCount));
@@ -40,6 +42,7 @@ class NhnMartShell {
         }
 
         return bl;
+        // return buyList;
     }
 }
 
