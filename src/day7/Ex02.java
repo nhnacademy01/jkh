21cb1d24e278631dfbb00dc6b1da8559aa917c7c
jungkihun <khkh0130@gmail.com>
2022-03-15 11:27:42 +0900

    jkh day7 Ex04

--- src/day7/Ex02.java
+++ src/day7/Ex02.java
@@ -7,6 +7,17 @@ public class Ex02 {
     public static void main(String[] args) {
         Ex02 i = new Ex02();
         i.satisfy();
+
+        Pattern p = Pattern.compile("([a-zA-Z0-9_.+-]+)@([a-zA-Z0-9-]+\\.[a-zA-Z0-9-.]+)");
+        Matcher m = p.matcher("이메일은 hong@nhn.com 입니다.");
+        while (m.find()) {
+            System.out.println("m.groupCount(): " + m.groupCount());
+            System.out.println("m.group(): " + m.group());  // m.group(0)
+            System.out.println("m.group(1): " + m.group(1));
+            System.out.println("m.group(2): " + m.group(2));
+        }
+        i.replace();
+        i.number();
     }
     public void satisfy(){
         Pattern p = Pattern.compile("^c.+");//c로 시작하는 문자열
@@ -17,4 +28,30 @@ public class Ex02 {
             System.out.println("Not starts with 'c'");
         }
     }
+    private void replace() {
+        Pattern p = Pattern.compile("([a-zA-Z0-9_.+-]+)@([a-zA-Z0-9-]+\\.[a-zA-Z0-9-.]+)");
+        Matcher m = p.matcher("이메일은 hong@nhn.com 입니다.");
+        StringBuffer result = new StringBuffer();
+        while (m.find()) {
+            String localPart = m.group(1);
+            m.appendReplacement(result, localPart + "@nhnacademy.com");
+        }
+        m.appendTail(result);
+        System.out.println(result);
+    }
+    private void number(){
+        Pattern p = Pattern.compile("\\s(031)");
+        Matcher m = p.matcher("032-0032-0320\n" +
+                "032-0031-0311\n" +
+                "031-031-0310\n" +
+                "031-031-0322");
+        StringBuffer result = new StringBuffer();
+        while (m.find()){
+            String localPart = m.group(1);
+            m.appendReplacement(result,"\n032");
+        }
+        m.appendTail(result);
+        System.out.println(result);
+    }
+
 }
---
+++ src/day7/Ex04.java
@@ -0,0 +1,25 @@
+package day7;
+
+import java.util.StringTokenizer;
+
+public class Ex04 {
+    public static void main(String[] args) {
+        String input = "50,20,,,40,30";
+        // split
+        String[] tokens = input.split(",");
+        int countBySplit = tokens.length;
+        System.out.println("countBySplit: " + countBySplit);
+        System.out.println("tokensBySplit");
+        for (String token : tokens) {
+            System.out.printf("'%s' ", token);
+        }
+        // StringTokenizer
+        StringTokenizer st = new StringTokenizer(input, ",");
+        int countBySt = st.countTokens();
+        System.out.printf("%n%ncountBySt: %d%n", countBySt);
+        System.out.println("tokensBySt");
+        while (st.hasMoreTokens()) {
+            System.out.printf("'%s' ", st.nextToken());
+        }
+    }
+}
